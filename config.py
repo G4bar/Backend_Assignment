21c0792ae184ebe3753ce042dee413c8fa0b922a
gabar <gabrieleberti18@gmail.com>
2025-06-13 21:41:12 +0200

    Refactor configuration to remove default values for environment variables and delete unused database file

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", 'sqlite:///polling.db')
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "jwtsecret")
-    ADMIN_SECRET = os.environ.get("ADMIN_SECRET", "default_admin_secret")
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+    ADMIN_SECRET = os.environ.get("ADMIN_SECRET")
